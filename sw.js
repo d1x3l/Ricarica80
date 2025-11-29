e0844c7ae8e196817e61d3619c63e82d543b8149
diego <118854031+d1x3l@users.noreply.github.com>
2025-11-29 19:40:37 +0100

    Refactor activate event listener for clarity

--- sw.js
+++ sw.js
@@ -4,9 +4,8 @@ self.addEventListener('install', e => {
 
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
-      .then(() => self.clients.claim())
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+    .then(() => self.clients.claim())
   );
 });
 
